f359c7b4333c7a437b66c04b032e7ec102ed0547
Erick Freire <erilefr@gmail.com>
2021-03-25 22:55:59 -0300

    BemVindo3
    BemVindo3

---
+++ BemVindo3/src/br/com/erickfreire/bemvindo3/BemVindo3.java
@@ -0,0 +1,17 @@
+package br.com.erickfreire.bemvindo3;
+
+/**
+ * Programa que exibe uma mensagem de boas vindas com quebra de linha
+ * @author Erick Freire
+ * @version 1 - Criado em 25-03-2021 as 22:54
+ */
+
+public class BemVindo3 {
+
+	public static void main(String[] args) {
+		
+		System.out.println("Bem Vindo\na\nProgramação\nem\nJava!");
+
+	}
+
+}
